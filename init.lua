e43e57c45af06a1e09bb8b8a45c13ec79766eddc
FlyOverHere <44926347+FlyOverHere@users.noreply.github.com>
2018-11-10 18:28:35 +0300

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,6 @@
+minetest.register_node("whogran:palegranite", {
+    description = "Pale Granite",
+    tiles = {" homedecor_kitchen_cabinet_top_granite.png "},
+    is_ground_content = true,
+    groups = {cracky=3, stone=1}
+})
