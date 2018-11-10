534e9db358ca56d75f2fc18b53185df123a9985b
FlyOverHere <44926347+FlyOverHere@users.noreply.github.com>
2018-11-10 18:41:28 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ minetest.register_node("fewbuildingblocks:palegranite", {
     description = "Pale Granite",
     tiles = {" homedecor_kitchen_cabinet_top_granite.png "},
     is_ground_content = true,
-    paramtype2 = "facedir"
+    paramtype2 = "facedir",
     groups = {cracky=3, stone=1}
 })
